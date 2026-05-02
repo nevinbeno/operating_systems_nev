0e3f71d575d24004abf4e9ca4ff22312ac36649d
Nevin Beno <206091677+nevinbeno@users.noreply.github.com>
2026-05-02 16:02:28 +0530

    lru in c++

--- Page Replacement Algorithms/lru.cpp
+++ Page Replacement Algorithms/lru.cpp
@@ -2,11 +2,58 @@
 #include <unordered_map>
 #include <algorithm>
 #include <vector>
-
+#include <list>
 using namespace std;
+
+void lru_algorithm(int pages, vector<int>& req, int frames)
+{
+    list<int> dll;
+    unordered_map<int, list<int>::iterator> hash_table;
+    for (int i = 0; i < pages; i++)
+    {
+        int key = req[i];
+        auto it = hash_table.find(key);
+        if (it != hash_table.end()) // there is a match in the hash table
+        {
+            dll.splice(dll.begin(), dll, it->second);
+            it->second = dll.begin();
+        }
+        else // not in hash table
+        {
+            if (dll.size() >= frames)
+            {
+                int value_of_popped = dll.back();
+                hash_table.erase(value_of_popped);
+                dll.pop_back();
+            }
+                dll.push_front(key);
+                hash_table[key] = dll.begin();
+        }
+    }
+    cout<<"The final configuration of the frames: "<<endl;
+    for (int element : dll)
+        cout<<element<<" ";
+    cout<<endl;
+}
 
 int main ()
 {
+    int no_of_pages;
+    cout<<"Enter the number of pages: ";
+    cin>>no_of_pages;
     
+    vector<int> request(no_of_pages);
+    cout<<"Enter the pages: "<<endl;
+    for (int i = 0; i < no_of_pages; i++)
+    {
+        cout<<"Page "<<i + 1<<": ";
+        cin>>request[i];
+    }
+
+    int no_of_frames;
+    cout<<"Enter the no. of frames: ";
+    cin>>no_of_frames;
+
+    lru_algorithm(no_of_pages, request, no_of_frames);
     return 0;
 }
