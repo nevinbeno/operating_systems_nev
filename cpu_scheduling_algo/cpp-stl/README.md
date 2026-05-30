817ca30a80eb53b9a53ad0be32ef1eff19c386ac
Nevin Beno <206091677+nevinbeno@users.noreply.github.com>
2026-05-30 08:59:06 +0530

    srtf cpp

---
+++ cpu_scheduling_algo/cpp-stl/README.md
@@ -0,0 +1,20 @@
+# CPU Scheduling (using C++ STL)
+
+## SRTF Algorithm
+```txt
+1. Start.
+2. Sort the array of struct based of arrival time.
+3. Shifting of time axis to the p[0].at
+4. Repeat till all processes are completed.
+    4.1. Find all process that have arived by the time.
+    4.2. Add those processes into the queue. 
+    4.3. If queue is empty (CPU idle), increment time.
+    4.4. Else:
+        4.4.1. Access the front element from the queue. 
+        4.4.2. Decrement 1 unit of remaining time. 
+        4.4.3. Increment time. 
+        4.4.4. Calculate ct, rt, etc... if suitable.
+        4.4.5. Increment "completed" if suitable. 
+5. Display the specs
+6. Stop. 
+```
---
+++ cpu_scheduling_algo/cpp-stl/srtf.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <deque>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct process
+{
+    int id;
+    int at;
+    int bt;
+    int rem;
+    int ct;
+    int tat;
+    int wt;
+    int rt;
+    bool touched;
+    bool first_processing;
+};
+
+bool compare_at(const struct process& a, const struct process& b)
+{
+    return a.at < b.at;
+}
+
+bool compare_rem(const struct process* a, const process* b)
+{
+    return a->rem < b->rem;
+}
+
+bool compare_id(const struct process& a, const struct process& b)
+{
+    return a.id < b.id;
+}
+
+bool srtf(vector<struct process>& p)
+{
+    int n_proc = p.size();
+    if (n_proc < 1)
+        return false;
+
+    deque<struct process*> q;
+    int time = 0, completed = 0;
+
+    time += p[0].at;
+
+    for (int i = 0; i < n_proc; i++)
+    {
+        p[i].rem = p[i].bt;
+        p[i].touched = false;
+        p[i].first_processing = true;
+        if (p[i].rem == 0)
+            completed++;
+    }
+
+    sort(p.begin(), p.end(), compare_at);
+
+    while(completed < n_proc)
+    {
+        for (int i = 0; i < n_proc; i++)
+            if (p[i].at <= time && p[i].touched == false && p[i].rem > 0)
+            {
+                q.push_back(&p[i]);
+                p[i].touched = true;
+            }
+        if (q.empty())
+            time++;
+        else
+        {
+            sort(q.begin(), q.end(), compare_rem);
+            struct process* current = q.front();
+            current->rem--;
+            if (current->first_processing)
+            {
+                current->first_processing = false;
+                current->rt = time - current->at;
+            }
+            time++;
+            if (current->rem == 0)
+            {
+                current->ct = time;
+                current->tat = current->ct - current->at;
+                current->wt = current->tat - current->bt;
+                q.pop_front();
+                completed++;
+            }
+        }
+    }
+
+    return true;
+}
+
+void display(vector<struct process>& p)
+{
+    sort(p.begin(), p.end(), compare_id);
+    int total_elements = p.size();
+    cout<<"== Shortest Remaining Time First =="<<endl;
+    cout<<"PID\tAT\tBT\tCT\tTAT\tWT\tResponse Time"<<endl;
+    for (int i = 0; i < total_elements; i++)
+        cout<<p[i].id<<"\t"<<p[i].at<<"\t"<<p[i].bt<<"\t"<<p[i].ct<<"\t"<<p[i].tat<<"\t"<<p[i].wt<<"\t"<<p[i].rt<<endl;
+    return;
+}
+
+int main ()
+{
+    vector<struct process> p;
+    int n;
+    cout<<"Enter the no. of processes: ";
+    cin>>n;
+
+    cout<<"Enter those process details: "<<endl;
+    for (int i = 0; i < n; i ++)
+    {
+        struct process temp;
+        temp.id = i;
+        cout<<"Process "<<i<<": "<<endl;
+        cout<<"Arrival time: ";
+        cin>>temp.at;
+        cout<<"Burst time: ";
+        cin>>temp.bt;
+        p.push_back(temp);
+        cout<<endl;
+    } 
+
+    bool answer = srtf(p);
+
+    cout<<endl;
+
+    if (answer)
+        display(p);
+    else
+        cout<<"Error occurred.."<<endl;
+    return 0;
+}
